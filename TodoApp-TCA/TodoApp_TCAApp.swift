c8f23e9254e9866e4377bcd3585df6f401b4ccf1
Yugo Matsuda <yugo1997zazen@gmail.com>
2024-03-04 21:17:34 +0900

    Initial Commit

---
+++ TodoApp-TCA/TodoApp_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp_TCAApp.swift
+//  TodoApp-TCA
+//
+//  Created by Yugo Matsuda on 2024-03-04.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
